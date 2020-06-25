002161be0b19df7ea134d61c91cafd03c14bc36e
agarwalarpit99 <arpitagarwal990@gmail.com>
2020-06-25 20:13:43 +0530

    All new

--- updatedaac/urls.py
+++ updatedaac/urls.py
@@ -2,6 +2,10 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('index', views.index,name="index"),
+    path('', views.index,name="index"),
     path('about',views.about,name="about"),
+    path('custom',views.custom,name='custom'),
+    path('infrastructure',views.infrastructure,name="infrastructure"),
+    path('contact',views.contact,name="contact"),
 ]
+#class="container"  data-spy="scroll" data-target="#navbar-example2" data-offset="0" id="about"
--- updatedaac/views.py
+++ updatedaac/views.py
@@ -5,3 +5,9 @@ def index(request):
     return render(request,'index.html')
 def about(request):
     return render(request,'about.html')
+def custom(request):
+    return render(request,'custom.html')
+def infrastructure(request):
+    return render(request,'infrastructure.html')
+def contact(request):
+    return render(request,'contact.html')
